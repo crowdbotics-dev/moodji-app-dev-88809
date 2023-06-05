dd6a36c282e3f51b21222184e646a95bc7f15add
Aline Tavares <aline@crowdbotics.com>
2023-06-05 13:04:02 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "moodji-app-dev-88809",
   url: "https://moodji-app-dev-88809.botics.co",
   api: "https://moodji-app-dev-88809.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
