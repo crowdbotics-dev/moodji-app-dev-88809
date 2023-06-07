0b6424ef8c274b88759b18e3f90973d480062ad9
Aline Tavares <aline@crowdbotics.com>
2023-06-07 14:05:36 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "moodji-app-dev-88809",
-  url: "https://moodji-app-dev-88809.botics.co",
-  api: "https://moodji-app-dev-88809.botics.co/api/v1"
+  "name": "moodji-app-dev-88809",
+  "url": "https://moodji-app-dev-88809.botics.co",
+  "api": "https://moodji-app-dev-88809.botics.co/api/v1",
+  "initialRoute": "ScreenAI2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
