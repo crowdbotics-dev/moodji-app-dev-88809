ded58cdce5f640f5fe89004c34695a436080a0c7
Aline Tavares <alaisgomes@gmail.com>
2023-06-12 11:36:13 -0400

    Fix dispatch code

--- screens/ScreenAI3/index.js
+++ screens/ScreenAI3/index.js
@@ -1,33 +1,67 @@
-import { loginRequest } from "../../modules/login/auth/index.js";
-import { useDispatch } from "react-redux";
-import React, { useState } from "react";
-import { View, Text, TextInput, Pressable, Image, StyleSheet, Alert } from "react-native";
+import { api_v1_login_create } from "../../store/moodjiappdevAPI/authTokens.slice.js"
+import { useDispatch } from "react-redux"
+import React, { useState } from "react"
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Image,
+  StyleSheet,
+  Alert
+} from "react-native"
 
-const LoginScreen = ({
-  navigation
-}) => {
-  const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  return <View style={styles.container}>
-      <Image source={require("./Pink Blue Cute Illustration Cat Adoption Logo.png")} style={styles.logo} resizeMode="cover" />
-      <TextInput style={styles.input} onChangeText={setEmail} value={email} placeholder="Email" keyboardType="email-address" autoCapitalize="none" clearTextOnFocus={true} enablesReturnKeyAutomatically={true} rejectResponderTermination={true} scrollEnabled={true} />
-      <TextInput style={styles.input} onChangeText={setPassword} value={password} placeholder="Password" secureTextEntry />
+const LoginScreen = ({ navigation }) => {
+  const dispatch = useDispatch()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const onSubmit = () => {
+    dispatch(
+      api_v1_login_create({
+        email,
+        password
+      })
+    )
+      .then(() => {
+        const result = unwrapResult(response)
+        navigation.navigate("Untitled4", result)
+      })
+      .catch(() => Alert.alert("Something went wrong!"))
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("./logo.png")}
+        style={styles.logo}
+        resizeMode="cover"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        clearTextOnFocus={true}
+        enablesReturnKeyAutomatically={true}
+        rejectResponderTermination={true}
+        scrollEnabled={true}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Password"
+        secureTextEntry
+      />
       <Pressable style={styles.button} onPress={onSubmit}>
         <Text style={styles.buttonText}>Login</Text>
       </Pressable>
-    </View>;
-
-  const onSubmit = () => {
-    dispatch(loginRequest({
-      email,
-      password
-    }).then(() => {
-      const result = unwrapResult(response);
-      navigation.navigate("Untitled4", result);
-    }).catch(() => Alert.alert("Something went wrong!")));
-  };
-};
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -61,5 +95,5 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold"
   }
-});
-export default LoginScreen;
+})
+export default LoginScreen
