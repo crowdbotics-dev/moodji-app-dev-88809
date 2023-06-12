895579a3147e46a623c6294032452114813ebf50
Aline Tavares <alaisgomes@gmail.com>
2023-06-12 15:49:32 -0400

    test

--- store/config.js
+++ store/config.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { Platform } from "react-native"
+console.log(Platform.OS)
 
 if (axios && Platform.OS === "web") {
   axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
