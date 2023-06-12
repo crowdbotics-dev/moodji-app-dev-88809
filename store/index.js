4e9155e5f7478fd280c1e659abb78e5da07ca453
Aline Tavares <alaisgomes@gmail.com>
2023-06-12 16:15:47 -0400

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,5 +1,6 @@
-import storeSlices from "./*/*.slice.js"; // Minimal check to see if imported slice has all properties of an actual slice
 import "./config.js";
+import storeSlices from "./*/*.slice.js"; // Minimal check to see if imported slice has all properties of an actual slice
+
 
 const isValid = slice => {
   const sliceProps = ["actions", "caseReducers", "name", "reducer", "getInitialState"];
