32c024e3a7309d6139f956b26c9240457f7dffc3
Ravinder sales <ravinder.y@crowdbotics.com>
2023-06-22 12:48:42 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/config.js
+++ store/config.js
@@ -1,9 +1,9 @@
-import axios from "axios"
-import { Platform } from "react-native"
-console.log(Platform.OS)
+import axios from "axios";
+import { Platform } from "react-native";
+console.log(Platform.OS);
 
 if (axios && Platform.OS === "web") {
-  axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
-  axios.defaults.xsrfCookieName = "csrftoken"
-  axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest"
+  axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+  axios.defaults.xsrfCookieName = "csrftoken";
+  axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 }
--- store/index.js
+++ store/index.js
@@ -1,7 +1,6 @@
 import "./config.js";
 import storeSlices from "./*/*.slice.js"; // Minimal check to see if imported slice has all properties of an actual slice
 
-
 const isValid = slice => {
   const sliceProps = ["actions", "caseReducers", "name", "reducer", "getInitialState"];
   return Object.keys(slice).every(prop => sliceProps.includes(prop));
