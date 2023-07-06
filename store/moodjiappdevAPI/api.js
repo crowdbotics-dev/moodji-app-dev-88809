4a6224feed66a6ea3dfc400df76d9b2070d4e633
Aline Tavares <alaisgomes@gmail.com>
2023-07-06 10:09:48 -0400

    Update store.

--- store/moodjiappdevAPI/api.js
+++ store/moodjiappdevAPI/api.js
@@ -3,19 +3,28 @@ const moodjiappdevAPI = axios.create({
   baseURL: "https://moodji-app-dev-88809.botics.co",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function api_v1_login_create(payload) {
+  return moodjiappdevAPI.post(`/api/v1/login/`, payload)
+}
+function api_v1_signup_create(payload) {
+  return moodjiappdevAPI.post(`/api/v1/signup/`, payload)
+}
+function rest_auth_user_retrieve(payload) {
+  return moodjiappdevAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return moodjiappdevAPI.put(`/rest-auth/user/`, payload)
+}
+function rest_auth_user_partial_update(payload) {
+  return moodjiappdevAPI.patch(`/rest-auth/user/`, payload)
+}
 function api_docs_schema_retrieve(payload) {
   return moodjiappdevAPI.get(`/api-docs/schema/`, {
     params: { lang: payload.lang }
   })
 }
-function api_v1_login_create(payload) {
-  return moodjiappdevAPI.post(`/api/v1/login/`, payload.data)
-}
-function api_v1_signup_create(payload) {
-  return moodjiappdevAPI.post(`/api/v1/signup/`, payload.data)
-}
 function rest_auth_login_create(payload) {
-  return moodjiappdevAPI.post(`/rest-auth/login/`, payload.data)
+  return moodjiappdevAPI.post(`/rest-auth/login/`, payload)
 }
 function rest_auth_logout_retrieve(payload) {
   return moodjiappdevAPI.get(`/rest-auth/logout/`)
@@ -23,49 +32,34 @@ function rest_auth_logout_retrieve(payload) {
 function rest_auth_logout_create(payload) {
   return moodjiappdevAPI.post(`/rest-auth/logout/`)
 }
-function rest_auth_password_change_create(payload) {
-  return moodjiappdevAPI.post(`/rest-auth/password/change/`, payload.data)
+function rest_auth_registration_create(payload) {
+  return moodjiappdevAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_password_reset_create(payload) {
-  return moodjiappdevAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return moodjiappdevAPI.post(`/rest-auth/password/reset/`, payload)
+}
+function rest_auth_password_change_create(payload) {
+  return moodjiappdevAPI.post(`/rest-auth/password/change/`, payload)
 }
 function rest_auth_password_reset_confirm_create(payload) {
-  return moodjiappdevAPI.post(
-    `/rest-auth/password/reset/confirm/`,
-    payload.data
-  )
-}
-function rest_auth_registration_create(payload) {
-  return moodjiappdevAPI.post(`/rest-auth/registration/`, payload.data)
+  return moodjiappdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
 function rest_auth_registration_verify_email_create(payload) {
-  return moodjiappdevAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
-}
-function rest_auth_user_retrieve(payload) {
-  return moodjiappdevAPI.get(`/rest-auth/user/`)
-}
-function rest_auth_user_update(payload) {
-  return moodjiappdevAPI.put(`/rest-auth/user/`, payload.data)
-}
-function rest_auth_user_partial_update(payload) {
-  return moodjiappdevAPI.patch(`/rest-auth/user/`, payload.data)
+  return moodjiappdevAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
 export const apiService = {
-  api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update,
+  api_docs_schema_retrieve,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
-  rest_auth_password_change_create,
-  rest_auth_password_reset_create,
-  rest_auth_password_reset_confirm_create,
   rest_auth_registration_create,
-  rest_auth_registration_verify_email_create,
-  rest_auth_user_retrieve,
-  rest_auth_user_update,
-  rest_auth_user_partial_update
+  rest_auth_password_reset_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_verify_email_create
 }
